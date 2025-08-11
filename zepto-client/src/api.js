5fa016db1d292505c5512a8b210bc1f6aeb43116
manudevops726 <amanasa726@gmail.com>
2025-08-11 23:40:51 +0530

    Update api.js

--- zepto-client/src/api.js
+++ zepto-client/src/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http:// 54.209.182.187:3001',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}` // Or however you store it
   }
