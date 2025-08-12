4cd206755721f7e0714ea280838a1db3fefc7849
manudevops726 <amanasa726@gmail.com>
2025-08-12 14:26:08 +0530

    Update index.js

--- zepto-backend/src/models/index.js
+++ zepto-backend/src/models/index.js
@@ -7,15 +7,6 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'const { Sequelize, DataTypes } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
   }
@@ -24,25 +15,7 @@ const sequelize = new Sequelize(
 const User = sequelize.define('User', {
   mobile: { type: DataTypes.STRING, allowNull: false, unique: true },
   otp: DataTypes.STRING,
-  verified: DataTypes.BOOLEAN,
-});
-
-const Product = sequelize.define('Product', {
-  name: DataTypes.STRING,
-  image: DataTypes.STRING,
-  price: DataTypes.INTEGER,
-  addedBy: DataTypes.STRING,
-});
-
-module.exports = { sequelize, User, Product };
-
-  }
-);
-
-const User = sequelize.define('User', {
-  mobile: { type: DataTypes.STRING, allowNull: false, unique: true },
-  otp: DataTypes.STRING,
-  verified: DataTypes.BOOLEAN
+  verified: { type: DataTypes.BOOLEAN, defaultValue: false }
 });
 
 const Product = sequelize.define('Product', {
