848a03145181e14d9eebfcee9db0df285eb36ce9
manudevops726 <amanasa726@gmail.com>
2025-08-12 14:40:46 +0530

    Update api.js

--- zepto-client/src/api.js
+++ zepto-client/src/api.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   // baseURL: 'http://<YOUR_BACKEND_IP>:3001'  // 🔁 Replace with actual IP or domain
-//   baseURL: 'http://localhost:3001'  // 🔁 Replace with actual IP or domain
-// });
-
 const api = axios.create({
-  baseURL: 'http://54.209.182.187:3001',
+  baseURL: 'http://54.209.182.187:3001', // Replace with your backend IP or domain
   headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}` // Or however you store it
+    Authorization: `Bearer ${localStorage.getItem('token') || ''}`
   }
 });
 
