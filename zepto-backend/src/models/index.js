3b339ed8ad38488bc74820a1100316ce397e4b11
manudevops726 <amanasa726@gmail.com>
2025-08-12 14:14:39 +0530

    Update index.js

--- zepto-backend/src/models/index.js
+++ zepto-backend/src/models/index.js
@@ -7,7 +7,35 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql'const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
+
+const User = sequelize.define('User', {
+  mobile: { type: DataTypes.STRING, allowNull: false, unique: true },
+  otp: DataTypes.STRING,
+  verified: DataTypes.BOOLEAN,
+});
+
+const Product = sequelize.define('Product', {
+  name: DataTypes.STRING,
+  image: DataTypes.STRING,
+  price: DataTypes.INTEGER,
+  addedBy: DataTypes.STRING,
+});
+
+module.exports = { sequelize, User, Product };
+
   }
 );
 
