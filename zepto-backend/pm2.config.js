00a86e610bac918796906977f723f165516c3e1b
manudevops726 <amanasa726@gmail.com>
2025-08-21 15:25:24 +0530

    Update pm2.config.js

--- zepto-backend/pm2.config.js
+++ zepto-backend/pm2.config.js
@@ -2,14 +2,17 @@ module.exports = {
   apps: [
     {
       name: "zepto-backend",
-      script: "./app.js",
-      watch: true,
+      script: "app.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "development"
       },
       env_production: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+        NODE_ENV: "production"
+      }
+    }
+  ]
 };
